42cfe360280f15e0707700d6a446854b82a94cd6
ramadhangandidi <thinkpad1st@gmail.com>
2022-11-23 08:21:30 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Halo, nama saya {{ nama }}</h1>
+    <h2>Kalau saya {{ anggota[0] + " " + anggota[1] }}</h2>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <button 
+      v-on:click="gantiNama()">
+      Ganti Nama</button>
+
+    <h2>{{ totalBelanja }}</h2>
+    <button v-on:click="gantiQuantity()">Ganti Qty</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data: function(){
+    return{
+      nama : "Kodehive",
+      anggota : ["Bayu", "Eka", "Rangga"],
+      quantity : 5
+    }
+  },
+  methods: {
+    gantiNama : function(){
+      if(this.nama == "Kodehive"){
+        this.nama = "Adhan"
+      }else{
+        this.nama = "Kodehive"
+      }
+    },
+    gantiQuantity : function(){
+      this.quantity++
+    }
+  },
+  computed: {
+    totalBelanja : function(){
+      return this.quantity * 10000
+    }
+  },
+
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
